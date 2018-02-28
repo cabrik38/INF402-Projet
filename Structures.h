643e2269b7b916db2e7b7ed6b3699d16213beb48
madoci <36835658+madoci@users.noreply.github.com>
2018-02-28 23:33:33 +0100

    Create Structures.h

---
+++ Structures.h
@@ -0,0 +1,32 @@
+#ifndef H_STRUCTURES
+#define H_STRUCTURES
+
+
+/* -----------------------------------------------------------
+    (x, y) = coordonnées de la variable sur la grille
+    k = valeur particulière de la case (de 1 à n)
+    N = 1 s'il s'agit de la négation de la variable, 0 sinon 
+   ----------------------------------------------------------- */
+typedef struct Variable {
+    int x;
+    int y;
+    int k;
+    int N;
+} Variable;
+
+typedef enum NatureLexeme { TOP, BOTTOM, VARIABLE, CONJONCTION, DISJONCTION } NatureLexeme;
+
+typedef struct Lexeme {
+    struct NatureLexeme nature;
+    struct Variable var;
+} Lexeme;
+
+typedef struct Noeud {
+    Lexeme l;
+    struct Noeud* gauche;
+    struct Noeud* droit;
+} Noeud;
+
+typedef struct Noeud* Arbre;
+
+#endif
