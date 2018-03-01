05f01fb42f638d0957f933332cd1614102c81ebd
madoci <36835658+madoci@users.noreply.github.com>
2018-03-01 22:31:45 +0100

    Update Fnc.h

--- Fnc.h
+++ Fnc.h
@@ -8,9 +8,17 @@ typedef struct Clause {
     struct Clause* suivant;
 } Clause;
 
-typedef Clause* Liste;
+typedef struct Liste {
+    int longueur;
+    Clause* tete;
+    Clause* queue;
+} Liste;
 
 Liste clauses(Arbre A);
 int fnc(Arbre A);
+
+Clause* creer_clause(Arbre A);
+void ajouter_clause(Liste* list, Clause* cl);
+void ajouter_liste(Liste* list, Liste cls);
 
 #endif
