41eb65a1a9468a1927335a2c8570bb780ce28952
madoci <36835658+madoci@users.noreply.github.com>
2018-03-02 18:51:34 +0100

    Update Formules.h

--- Formules.h
+++ Formules.h
@@ -28,9 +28,28 @@ typedef struct Noeud {
 
 typedef struct Noeud* Arbre;
 
+/* --------------------
+    creer_variable(i,j,k,neg)
+    > Créer et renvoie un Arbre, constitué d'un seul noeud contenant la variable Xijk de coordonnées (i,j) et de valeur k.
+      Si neg = 1, c'est la négation de cette variable qui est représentée.
+   -------------------- */
 Arbre creer_variable(int i, int j, int k, int neg);
+
+/* --------------------
+    creer_conjonction(A1,A2)
+    > Renvoie l'arbre constitué de la conjonction des arbres A1 et A2 
+      La racine de l'arbre est une CONJONCTION et ses enfants gauche et droit sont respectivement A1 et A2.
+   -------------------- */
 Arbre creer_conjonction(Arbre A1, Arbre A2);
+
+/* --------------------
+    creer_disjonction(A1,A2)
+    > Renvoie l'arbre constitué de la disjonction des arbres A1 et A2 
+      La racine de l'arbre est une DISJONCTION et ses enfants gauche et droit sont respectivement A1 et A2.
+   -------------------- */
 Arbre creer_disjonction(Arbre A1, Arbre A2);
+
+
 Arbre creer_noeud();
 void liberer_arbre(Arbre A);
 
