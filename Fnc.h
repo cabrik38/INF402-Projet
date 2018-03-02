cb47950d893a4af6afa9fc4c20348d9783cc0bee
madoci <36835658+madoci@users.noreply.github.com>
2018-03-02 18:05:23 +0100

    Update Fnc.h

--- Fnc.h
+++ Fnc.h
@@ -14,10 +14,22 @@ typedef struct Liste {
     Clause* queue;
 } Liste;
 
-Liste clauses(Arbre A);
+/* --------------------
+    fnc(A)
+    Transforme une formule (exprimée sous la forme d'un arbre A) en forme normale conjonctive FNC.
+    Renvoie: -  1 si la formule obtenue est une FNC (avec au moins une conjonction)
+             -  0 s'il s'agit d'une clause (ou d'un littéral isolé).
+             - -1 en cas d'erreur
+   -------------------- */
 int fnc(Arbre A);
+/* --------------------
+    clauses(A)
+    Renvoie la liste des clauses d'une FNC (exprimée sous la forme d'un arbre A).
+   -------------------- */
+Liste clauses(Arbre A);
 
 Clause* creer_clause(Arbre A);
+void init_liste(Liste* list);
 void ajouter_clause(Liste* list, Clause* cl);
 void ajouter_liste(Liste* list, Liste cls);
 
