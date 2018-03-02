a56339504e29adaf7fde86ae0c60da4ea4b9497e
madoci <36835658+madoci@users.noreply.github.com>
2018-03-02 18:38:44 +0100

    Update Fnc.c

--- Fnc.c
+++ Fnc.c
@@ -5,8 +5,10 @@ Liste clauses(Arbre A){
     init_liste(&list);
     if (A != NULL){
         if (A->lex.nature != DISJONCTION){
+            /* Si A est une clause ou un littéral isolé, la clause créée à partir de A est ajoutée à la liste */
             ajouter_clause(&list, creer_clause(A));
         } else {
+            /* Si A est une FNC, la listes des clauses des enfant gauche et droit de A sont toutes deux ajoutées à la liste */
             ajouter_liste(&list, clauses(A->gauche));
             ajouter_liste(&list, clauses(A->droit));
         }
@@ -18,15 +20,24 @@ int fnc(Arbre A){
     if (A == NULL){
         return -1;
     }
-    if (A->lex.nature == VARIABLE){
+    if (A->lex.nature == VARIABLE || A->lex.nature == TOP || A->lex.nature == BOTTOM){
+        /* Si la formule est un littéral isolé, la fonction renvoie 0. */
         return 0;
     } else {
+        /* Si la formule est complexe (plusieurs littéraux), les sous-arbres gauche et droit sont transformés en FNC. */
         int g = fnc(A->gauche);
         int d = fnc(A->droit);
-        if (A->lex.nature == DISJONCTION){
+        if (A->lex.nature == CONJONCTION){
+            /* Si la racine de l'arbre A est une conjonction, la formule est déjà une FNC. */
+            return 1;
+        } else {
+            /* Si la racine de l'arbre A est une disjonction : */
             if (g == 0 && d == 0){
+                /* Si les sous-arbres gauche et droit représentent des clauses, alors l'arbre A représente une clause. */
                 return 0;
             } else if (g == 0 && d == 1){
+                /* A = G + D1...Dn      où G et Di sont des clauses
+                   A = (G + D1).(G + D2)...(G + Dn) */
                 Liste lc_d = clauses(A->droit);
                 Arbre Ag = A->gauche;
                 A = NULL;
@@ -38,6 +49,8 @@ int fnc(Arbre A){
                 }
                 return 1;
             } else if (g == 1 && d == 0){
+                /* A = G1...Gn + D      où Gi et D sont des clauses 
+                   A = (G1 + D).(G2 + D)...(Gn + D) */
                 Liste lc_g = clauses(A->gauche);
                 Arbre Ad = A->droit;
                 A = NULL;
@@ -49,6 +62,8 @@ int fnc(Arbre A){
                 }
                 return 1;
             } else if (g == 1 && d == 1){
+                /* A = G1...Gn + D1...Dn      où Gi et Di sont des clauses 
+                   A = (G1 + D1)...(G1 + Dn).(G2 + D1)...(G2 + Dn)...(Gn + Dn) */
                 Liste lc_g = clauses(A->gauche);
                 Liste lc_d = clauses(A->droit);
                 A = NULL;
@@ -64,10 +79,9 @@ int fnc(Arbre A){
                 }
                 return 1;
             } else {
+                /* Erreur lors de la transformation en FNC du sous-arbre gauche ou droit */
                 return -1;
             }
-        } else {
-            return 1;
         }
     }
 }
