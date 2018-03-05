23734dea410fb6156166ae1875b68d2cb3af0b11
madoci <36835658+madoci@users.noreply.github.com>
2018-03-05 21:15:05 +0100

    Update Fnc.c

--- Fnc.c
+++ Fnc.c
@@ -1,24 +1,28 @@
 #include "Fnc.h"
 
-int fnc(Arbre A){
+#include <stdlib.h>
+
+int fnc(Arbre *F){
+    Arbre A = *F;
+    int return_value;
     if (A == NULL){
         return -1;
     }
     if (A->lex.nature == VARIABLE || A->lex.nature == TOP || A->lex.nature == BOTTOM){
         /* Si la formule est un littéral isolé, la fonction renvoie 0. */
-        return 0;
+        return_value = 0;
     } else {
         /* Si la formule est complexe (plusieurs littéraux), les sous-arbres gauche et droit sont transformés en FNC. */
-        int g = fnc(A->gauche);
-        int d = fnc(A->droit);
+        int g = fnc(&(A->gauche));
+        int d = fnc(&(A->droit));
         if (A->lex.nature == CONJONCTION){
             /* Si la racine de l'arbre A est une conjonction, la formule est déjà une FNC. */
-            return 1;
+            return_value = 1;
         } else {
             /* Si la racine de l'arbre A est une disjonction : */
             if (g == 0 && d == 0){
                 /* Si les sous-arbres gauche et droit représentent des clauses, alors l'arbre A représente une clause. */
-                return 0;
+                return_value = 0;
             } else if (g == 0 && d == 1){
                 /* A = G + D1...Dn      où G et Di sont des clauses
                    A = (G + D1).(G + D2)...(G + Dn) */
@@ -31,9 +35,10 @@ int fnc(Arbre A){
                     A = creer_conjonction(A, D);
                     c = c->suivant;
                 }
-                return 1;
+                vider_liste(&lc_d);
+                return_value = 1;
             } else if (g == 1 && d == 0){
-                /* A = G1...Gn + D      où Gi et D sont des clauses 
+                /* A = G1...Gn + D      où Gi et D sont des clauses
                    A = (G1 + D).(G2 + D)...(Gn + D) */
                 Liste lc_g = clauses(A->gauche);
                 Arbre Ad = A->droit;
@@ -44,9 +49,10 @@ int fnc(Arbre A){
                     A = creer_conjonction(A, D);
                     c = c->suivant;
                 }
-                return 1;
+                vider_liste(&lc_g);
+                return_value = 1;
             } else if (g == 1 && d == 1){
-                /* A = G1...Gn + D1...Dn      où Gi et Di sont des clauses 
+                /* A = G1...Gn + D1...Dn      où Gi et Di sont des clauses
                    A = (G1 + D1)...(G1 + Dn).(G2 + D1)...(G2 + Dn)...(Gn + Dn) */
                 Liste lc_g = clauses(A->gauche);
                 Liste lc_d = clauses(A->droit);
@@ -61,20 +67,24 @@ int fnc(Arbre A){
                     }
                     cg = cg->suivant;
                 }
-                return 1;
+                vider_liste(&lc_g);
+                vider_liste(&lc_d);
+                return_value = 1;
             } else {
                 /* Erreur lors de la transformation en FNC du sous-arbre gauche ou droit */
                 return -1;
             }
         }
     }
+    *F = A;
+    return return_value;
 }
 
 Liste clauses(Arbre A){
-    List list;
+    Liste list;
     init_liste(&list);
     if (A != NULL){
-        if (A->lex.nature != DISJONCTION){
+        if (A->lex.nature != CONJONCTION){
             /* Si A est une clause ou un littéral isolé, la clause créée à partir de A est ajoutée à la liste */
             ajouter_clause(&list, creer_clause(A));
         } else {
@@ -93,44 +103,55 @@ Clause* creer_clause(Arbre A){
     return c;
 }
 
-void init_liste(Liste* list){
-    if (list == NULL){
+void init_liste(Liste* L){
+    if (L == NULL){
         return;
     }
-    list->longueur = 0;
-    list->tete = NULL;
-    list->queue = NULL;
+    L->longueur = 0;
+    L->tete = NULL;
+    L->queue = NULL;
 }
 
-void ajouter_clause(Liste* list, Clause* cl){
-    if (list == NULL || cl == NULL){
+void ajouter_clause(Liste* L, Clause* C){
+    if (L == NULL || C == NULL){
         return;
     }
-    if (list->longueur == 0){
-        list->tete = cl;
-    } else if (list->tete == NULL || list->queue == NULL){
+    if (L->longueur == 0){
+        L->tete = C;
+    } else if (L->tete == NULL || L->queue == NULL){
         return;
     } else {
-        list->queue->suivant = cl;
+        L->queue->suivant = C;
     }
-    list->queue = cl;
-    list->longueur++;
+    L->queue = C;
+    L->longueur++;
 }
 
-void ajouter_liste(Liste* list, Liste cls){
-    if (list == NULL){
+void ajouter_liste(Liste* L, Liste Cs){
+    if (L == NULL){
         return;
     }
-    if (cls.longueur == 0 || cls.tete == NULL || cls.queue == NULL){
+    if (Cs.longueur == 0 || Cs.tete == NULL || Cs.queue == NULL){
         return;
     }
-    if (list->longueur == 0){
-        list->tete = cls.tete;
-    } else if (list->tete == NULL || list->queue == NULL){
+    if (L->longueur == 0){
+        L->tete = Cs.tete;
+    } else if (L->tete == NULL || L->queue == NULL){
         return;
     } else {
-        list->queue->suivant = cls.tete;
+        L->queue->suivant = Cs.tete;
     }
-    list->queue = cls.queue;
-    list->longueur += cls.longueur;
+    L->queue = Cs.queue;
+    L->longueur += Cs.longueur;
+}
+
+void vider_liste(Liste* L){
+    Clause* c = L->tete;
+    Clause* next = NULL;
+    while (c != NULL){
+        next = c->suivant;
+        free(c);
+        c = next;
+    }
+    init_liste(L);
 }
