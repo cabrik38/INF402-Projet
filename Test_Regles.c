e2bdb382b7b2fbbc47d9f5e4915c7505eabfbad4
cabrik38 <32091280+cabrik38@users.noreply.github.com>
2018-03-05 16:48:40 +0100

    Update Test_Regles.c

--- Test_Regles.c
+++ Test_Regles.c
@@ -34,18 +34,25 @@ void afficher_formule(Arbre A){
 }
 
 int main(int argc, char** argv){
-  Arbre A1=regle_case(1,1,2,5);
+  printf("Arbre 1: \n");
+  Arbre A1=regle_case(1,1,5);
   afficher_formule(A1);
+  printf("Arbre 2: \n");
   Arbre A2=regle_ligne(2,3,4);
   afficher_formule(A2);
+  printf("Arbre 3: \n");
   Arbre A3=regle_ligne(6,3,9);
   afficher_formule(A3);
+  printf("Arbre 4: \n");
   Arbre A4=regle_colonne(3,4,5);
   afficher_formule(A4);
-  Arbre A5=regle_colonne(0,0,1);
+  printf("Arbre 5: \n");
+  Arbre A5=regle_colonne(0,1,1);
   afficher_formule(A5);
+  printf("Arbre 6: \n");
   Arbre A6=regle_sup(1,2,4,4,5);
   afficher_formule(A6);
+  printf("Arbre 7: \n");
   Arbre A7=regle_valeur(1,2,3);
   afficher_formule(A7);
   return 0;
