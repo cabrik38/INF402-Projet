97a655cad8c7b8b5162329973122e7d2e8b9ff71
cabrik38 <32091280+cabrik38@users.noreply.github.com>
2018-03-05 16:09:33 +0100

    Update and rename Test_regles.c to Test_Regles.c

--- Test_Regles.c
+++ Test_Regles.c
@@ -34,6 +34,19 @@ void afficher_formule(Arbre A){
 }
 
 int main(int argc, char** argv){
-  
+  Arbre A1=regle_case(1,1,2,5);
+  afficher_formule(A1);
+  Arbre A2=regle_ligne(2,3,4);
+  afficher_formule(A2);
+  Arbre A3=regle_ligne(6,3,9);
+  afficher_formule(A3);
+  Arbre A4=regle_colonne(3,4,5);
+  afficher_formule(A4);
+  Arbre A5=regle_colonne(0,0,1);
+  afficher_formule(A5);
+  Arbre A6=regle_sup(1,2,4,4,5);
+  afficher_formule(A6);
+  Arbre A7=regle_valeur(1,2,3);
+  afficher_formule(A7);
   return 0;
 }
