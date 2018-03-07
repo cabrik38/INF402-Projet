7017f645bf71259f1a8d9acd438d294e3c88298c
madoci <36835658+madoci@users.noreply.github.com>
2018-03-07 23:16:50 +0100

    Update Dimacs.h

--- Dimacs.h
+++ Dimacs.h
@@ -3,7 +3,13 @@
 
 #include "Formules.h"
 #include "Fnc.h"
+#include <stdio.h>
 
-void ecrire_dimacs(Arbre A, char* nom_fichier);
+void generer_dimacs(Arbre A, char* nom_fichier, int n);
+void ecrire_commentaire(char* commentaire, FILE* f);
+void ecrire_clause(Clause* C, FILE* f, int n);
+void ecrire_clause_rec(Arbre A, FILE* f, int n);
+int numero_variable(Variable V, int n);
+int nombre_variables(int n);
 
 #endif
