19800099a60b7a4cd185e997ad82318c2bda8517
cabrik38 <32091280+cabrik38@users.noreply.github.com>
2018-03-08 11:04:58 +0100

    Create Futoshiki.c

---
+++ Futoshiki.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include "Formules.h"
+#include "Lecture_Fichiers.h"
+#include "Dimacs.h"
+
+int main(int argc, char** argv){
+    if (argc != 3){
+        printf("Nombre d'arguments invalide : %s <fichier_entree> <fichier_sortie>\n", argv[0]);
+        return -1;
+    }
+    int n;
+    Arbre A = Lecture_grille(argv[1], &n);
+    generer_dimacs(A, argv[2], n);
+    return 0;
+}
