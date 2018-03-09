b889255159eb04ae9458a4d83831c8f4617a0c98
madoci <36835658+madoci@users.noreply.github.com>
2018-03-09 18:25:45 +0100

    Update Regles.h

--- Regles.h
+++ Regles.h
@@ -10,16 +10,16 @@
 Arbre regle_case(int i, int j, int n);
 
 /* ------------------------------
-    regle_ligne(i,k)
-    > Renvoie l'arbre correspondant à la formule logique Li (cf. Info.txt)
+    regle_colonne(i,k)
+    > Renvoie l'arbre correspondant à la formule logique Cik (cf. Info.txt)
    ------------------------------ */
-Arbre regle_ligne(int i, int k, int n);
+Arbre regle_colonne(int i, int k, int n);
 
 /* ------------------------------
-    regle_colonne(j,k)
-    > Renvoie l'arbre correspondant à la formule logique Cj (cf. Info.txt)
+    regle_ligne(j,k)
+    > Renvoie l'arbre correspondant à la formule logique Ljk (cf. Info.txt)
    ------------------------------ */
-Arbre regle_colonne(int j, int k, int n);
+Arbre regle_ligne(int j, int k, int n);
 
 /* ------------------------------
     regle_sup(a,b,c,d)
