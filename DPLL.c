f1d1d56ea4f9a170c62d4a83ce012ff7080990d8
madoci <36835658+madoci@users.noreply.github.com>
2018-04-23 14:05:51 +0200

    Update DPLL.c

--- DPLL.c
+++ DPLL.c
@@ -1,6 +1,6 @@
 #include "DPLL.h"
 
-#include "Formules.h"
+#include <stdlib.h>
 
 NoeudVar* creer_noeudVar(Variable v){
     NoeudVar* n = (NoeudVar*) malloc(sizeof(NoeudVar));
@@ -35,4 +35,56 @@ void liste_variable(ListeVar* L, Arbre A){
         liste_variable(L, A->gauche);
         liste_variable(L, A->droit);
     }
+}
+
+int estIdentique(Variable v1, Variable v2){
+    if ((v1.x == v2.x) && (v1.y == v2.y) && (v1.k == v2.k) && (v1.N == v2.N)){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int estNegation(Variable v1, Variable v2){
+    if ((v1.x == v2.x) && (v1.y == v2.y) && (v1.k == v2.k) && (v1.N != v2.N)){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int estValide(ListeVar* L){
+    if (L == NULL) { return -1; }
+    NoeudVar* ni = L->tete;
+    while (ni != NULL){
+        NoeudVar* nj = ni->suivant;
+        while (nj != NULL){
+            if (estNegation(ni->var, nj->var)){
+                return 1;
+            }
+            nj = nj->suivant;
+        }
+        ni = ni->suivant;
+    }
+    return 0;
+}
+
+int contient(ListeVar* Lc, ListeVar* Lb){
+    if (Lc == NULL || Lb == NULL){ return -1; }
+    NoeudVar* ni = Lc->tete;
+    while (ni != NULL){
+        int inclus = 0;
+        NoeudVar* nj = Lb->tete;
+        while (nj != NULL && inclus == 0){
+            if (estIdentique(ni->var, nj->var)){
+                inclus = 1;
+            }
+            nj = nj->suivant;
+        }
+        if (inclus == 0){
+            return 0;
+        }
+        ni = ni->suivant;
+    }
+    return 1;
 }
