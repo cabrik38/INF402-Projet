74eef4ab37926f19f216a371a991b1a003fcf5d3
cabrik38 <32091280+cabrik38@users.noreply.github.com>
2018-04-23 11:19:28 +0200

    Update DPLL.c

--- DPLL.c
+++ DPLL.c
@@ -1,18 +1,38 @@
 #include "DPLL.h"
 
+#include "Formules.h"
+
 NoeudVar* creer_noeudVar(Variable v){
     NoeudVar* n = (NoeudVar*) malloc(sizeof(NoeudVar));
+    n->var = v;
     n->suivant = NULL;
     return n;
 }
 
 void init_listeVar(ListeVar* L){
+    if (L == NULL){
+        return;
+    }
     L->longueur = 0;
     L->tete = NULL;
 }
 
 void ajouter_variable(ListeVar* L, Variable v){
+    if (L == NULL){
+        return;
+    }
     NoeudVar* n = creer_noeudVar(v);
     n->suivant = L->tete;
     L->tete = n;
+}
+
+void liste_variable(ListeVar* L, Arbre A){
+    if (A == NULL){
+        return;
+    } else if (A->nature == VARIABLE){
+        ajouter_variable(L, A->var);
+    } else {
+        liste_variable(L, A->gauche);
+        liste_variable(L, A->droit);
+    }
 }
