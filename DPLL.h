f04434dd579ad67afdaeef85952686e7dfbc9399
madoci <36835658+madoci@users.noreply.github.com>
2018-04-23 14:05:47 +0200

    Update DPLL.h

--- DPLL.h
+++ DPLL.h
@@ -1,6 +1,8 @@
 #ifndef H_DPLL
 #define H_DPLL
 
+#include "Formules.h"
+
 typedef struct NoeudVar {
     Variable var;
     struct NoeudVar *suivant;
@@ -14,5 +16,10 @@ typedef struct ListeVar {
 NoeudVar* creer_noeudVar(Variable v);
 void init_liste(ListeVar* L);
 void ajouter_variable(ListeVar* L, Variable v);
+void liste_variable(ListeVar* L, Arbre A);
+int estIdentique(Variable v1, Variable v2);
+int estNegation(Variable v1, Variable v2);
+int estValide(ListeVar* L);
+int contient(ListeVar* Lc, ListeVar* Lb);
 
 #endif
